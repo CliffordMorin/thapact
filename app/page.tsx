63afe3dba7e93030f16268b55076252ef8af3b33
Clifford Morin <cemorin21@gmail.com>
2024-12-12 18:30:39 -0800

    new stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import LandingPage from "./components/sections/LandingPage";
+import AboutSection from "./components/sections/AboutSection";
+
 export default function Home() {
   return (
-    <div>
+    <main>
       <h1>Coming soon</h1>
-    </div>
+      <Image
+        src="/fullSingerShot.png"
+        alt="Full Singer Shot"
+        width={1440}
+        height={1033}
+      />
+      <LandingPage />
+      <AboutSection />
+    </main>
   );
 }
